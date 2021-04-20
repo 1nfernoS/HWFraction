6379e22effad3d5caa83d0da724d46fbd30f9d73
1nfernoS <gaydenko1801@gmail.com>
2021-04-21 02:41:04 +0300

    Make debug flag in settings

--- hw_api.py
+++ hw_api.py
@@ -1,7 +1,7 @@
 import requests
 
 from db.squads import get_token, get_squads
-from settings import fraction
+from settings import fraction, debug
 
 url = "https://aegis.hackerwars.ru/set_target"
 params = {"key": "466F2FFC", "source": "", "token": "", "target": 0}
@@ -14,9 +14,11 @@ def set_target(name, target):
     params["source"] = name
     params["target"] = target
     params["token"] = get_token(name)
-    # TODO: return to normal
-    print("request to " + url + " with " + str(params))
-    # requests.get(url, params)
+
+    if debug:
+        print("request to " + url + " with " + str(params))
+    else:
+        requests.get(url, params)
     return
 
 
@@ -25,16 +27,21 @@ def check(name, token):
     params["target"] = fraction
     params["token"] = token
     print("request to " + url + " with " + str(params))
-    # TODO: return to normal
-    return True
-    # r = requests.get(url, params)
-    # return 'Attack on yourself forbidden.' in str(r.content)
+
+    if debug:
+        return True
+    else:
+        r = requests.get(url, params)
+        return 'Attack on yourself forbidden.' in str(r.content)
 
 
 def distribute(msg="test"):
     params_dist['message']['message'] = msg
-    print("request to " + url + " with " + str(params))
-    # requests.post(url_dist, json=params_dist)
+
+    if debug:
+        print("request to " + url + " with " + str(params))
+    else:
+        requests.post(url_dist, json=params_dist)
     return
 
 
@@ -44,7 +51,10 @@ def remove_all():
         params["source"] = src
         params["target"] = 0
         params["token"] = get_token(src)
-        # TODO: return to normal
-        # requests.get(url, params)
-        print("request to " + url + " with " + str(params))
+
+        if debug:
+            print("request to " + url + " with " + str(params))
+        else:
+            requests.get(url, params)
+
     return
--- vk_api.py
+++ vk_api.py
@@ -1,34 +1,28 @@
 import vk
-import random
-from settings import token
+from settings import token, debug
 import json
 
-"""
 api = vk.API(vk.Session(), v='5.126')
 
 
 def send(user_id, msg, kbd=None):
+    if debug:
+        if kbd:
+            print(str(user_id) + ':\n' + str(msg) + ',\n kbd: True')
+        else:
+            print(str(user_id) + ':\n' + str(msg))
+        return
     if kbd:
         api.messages.send(access_token=token,
                           peer_id=str(user_id),
-                          random_id=random.getrandbits(64),
+                          random_id=0,
                           message=str(msg),
                           keyboard=json.dumps(kbd)
                           )
     else:
         api.messages.send(access_token=token,
                           peer_id=str(user_id),
-                          random_id=random.getrandbits(64),
+                          random_id=0,
                           message=str(msg)
                           )
-    return 
-"""
-
-
-# TODO: return to normal
-def send(user_id, msg, kbd=None):
-    if kbd:
-        print(str(user_id) + ':\n' + str(msg) + ',\n kbd: True')
-    else:
-        print(str(user_id) + ':\n' + str(msg))
-
+    return
